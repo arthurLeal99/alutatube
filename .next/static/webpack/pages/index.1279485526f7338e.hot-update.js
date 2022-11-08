92cab0c5ea1901eac8e4b43f5cab5ff32be1ca38
Arthur Leal <art5mussi@outlook.com>
2022-11-08 16:28:54 -0300

    first commit

---
+++ .next/static/webpack/pages/index.1279485526f7338e.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"name":"Arth Leal","job":"Aspirante a programador","github":"arthurLeal99","playlists":{"musicas":[{"title":"Michael Jackson - Beat It (Official Video)","url":"https://www.youtube.com/watch?v=oRdxUFDoQe0","thumb":"https://img.youtube.com/vi/oRdxUFDoQe0/hqdefault.jpg"},{"title":"Maniac","url":"https://www.youtube.com/watch?v=6GCNUeTFSbA","thumb":"https://img.youtube.com/vi/6GCNUeTFSbA/hqdefault.jpg"},{"title":"Cyberpunk: Edgerunners — Ending Theme | Let You Down by Dawid Podsiadło | Netflix","url":"https://www.youtube.com/watch?v=BnnbP7pCIvQ","thumb":"https://img.youtube.com/vi/BnnbP7pCIvQ/hqdefault.jpg"},{"title":"Tame Impala - The Less I Know the Better (Official Audio)","url":"https://www.youtube.com/watch?v=2SUwOgmvzK4","thumb":"https://img.youtube.com/vi/2SUwOgmvzK4/hqdefault.jpg"},{"title":"The Weeknd - Sacrifice (Audio)","url":"https://www.youtube.com/watch?v=E6zblNbGXA4","thumb":"https://img.youtube.com/vi/E6zblNbGXA4/hqdefault.jpg"},{"title":"The Score - Miracle (Official Video)","url":"https://www.youtube.com/watch?v=XW4vacvZSmA","thumb":"https://img.youtube.com/vi/XW4vacvZSmA/hqdefault.jpg"},{"title":"Imagine Dragons - Bones (Official Lyric Video)","url":"https://www.youtube.com/watch?v=DYed5whEf4g","thumb":"https://img.youtube.com/vi/DYed5whEf4g/hqdefault.jpg"}],"Podcast":[{"title":"LUCAS INUTILISMO & DIOGO DEFANTE - Podpah #129","url":"https://www.youtube.com/watch?v=h5P-kluDtGA","thumb":"https://img.youtube.com/vi/h5P-kluDtGA/hqdefault.jpg"},{"title":"THIAGO BRAGA (BRASÃO DE ARMAS E IMPÉRIOS AD) - Inteligência Ltda. Podcast #538","url":"https://www.youtube.com/watch?v=69vhQ2JyBVk","thumb":"https://img.youtube.com/vi/69vhQ2JyBVk/hqdefault.jpg"},{"title":"ED GAMA & NABOTE - Podpah #483","url":"https://www.youtube.com/watch?v=jRQkEA5EIF4","thumb":"https://img.youtube.com/vi/jRQkEA5EIF4/hqdefault.jpg"},{"title":"PODEROSÍSSIMO NINJA - Podpah #01","url":"https://www.youtube.com/watch?v=tqybzmT22ng","thumb":"https://img.youtube.com/vi/tqybzmT22ng/hqdefault.jpg"},{"title":"WHINDERSSON NUNES - Podpah #316","url":"https://www.youtube.com/watch?v=FRT4lI0Vp1o","thumb":"https://img.youtube.com/vi/jRQkEA5EIF4/hqdefault.jpg"},{"title":"MARIO SERGIO CORTELLA - Mais que 8 Minutos #129","url":"https://www.youtube.com/watch?v=RWs4K18ig-Q","thumb":"https://img.youtube.com/vi/FRT4lI0Vp1o/hqdefault.jpg"}]}}');
+
+/***/ })
+
+});
---
+++ .next/static/webpack/pages/index.41a1859a00a0b429.hot-update.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./config.json":
+/*!*********************!*\
+  !*** ./config.json ***!
+  \*********************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"name":"Arth Leal","job":"Aspirante a programador","github":"arthurLeal99","playlists":{"musicas":[{"title":"Michael Jackson - Beat It (Official Video)","url":"https://www.youtube.com/watch?v=oRdxUFDoQe0","thumb":"https://i.ytimg.com/vi/WlTlUseVt7E/hq720.jpg?sqp=-oaymwEXCNAFEJQDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDRQKDyCcoQVEr1Jk6Ir55i8rxlJQ"},{"title":"Algum ai 02","url":"url","thumb":"thumbnail do video"}],"games":[{"title":"game ai","url":"url","thumb":"thumbnail do video"},{"title":"game ai 02","url":"url","thumb":"thumbnail do video"}]}}');
+
+/***/ })
+
+});
